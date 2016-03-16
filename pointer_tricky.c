169d8d9d94c49f0289e91f94ea17f8a14f34c964
Abdulaziz Asiri <azizasiri90@gmail.com>
2016-03-16 16:57:59 -0700

    added another trick in pointer to pointer

--- pointer_tricky.c
+++ pointer_tricky.c
@@ -7,14 +7,12 @@ struct node * next ;
 };
 int main ()
 {
-	struct node *head= NULL; // List head is empty now 
-	struct node *temp = malloc(sizeof (struct node) ); 
-	temp->data = 12 ; 
-	temp->next = NULL; 
-
-/// WARNING : make sure to know the difference between 
-/// temp = NULL , and temp->next = NULL: 
-// temp = NULL ; means that the same node it is null |head|==0
-//temp->next= NULL, means check if the adjacent node is node |23 |-|200|(add:100)-->|5|-|0|(add:200)---->NULL, in this case yes 
-	//the next one points to NULL 
+	//struct node *head= NULL; // List head is empty now 
+	//struct node *temp = malloc(sizeof (struct node) ); 
+	//temp->data = 12 ; 
+	//temp->next = NULL; 
+int x = 5, *p=&x, **q=&p; 
+printf("%p\n",p); 
+printf("%p\n",&x);
+printf("%p\n",q);//This takes thee address of &p and *q will take address of x,p:)
 }
